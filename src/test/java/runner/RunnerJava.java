3b058837b810ecf27c8272e184037aa7e1e9a842
DianaMedina44 <dimedina44@misena.edu.co>
2025-05-23 22:37:56 -0500

    Correciones en Formulario, Pagina Base y Pasosformulario

--- src/test/java/runner/RunnerJava.java
+++ src/test/java/runner/RunnerJava.java
@@ -6,12 +6,15 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import pages.PaginaBase;
 
+// Indicamos que esta clase ejecuta pruebas con Cucumber
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources", 
     glue = "steps", 
     plugin = { "pretty", "html:target/cucumber-reports" })
 
+// Clase que se encarga de ejecutar los tests
 public class RunnerJava{
+    // Este método se ejecuta una vez al final de todos los escenarios
     @AfterClass
     public static void cleanDriver(){
         PaginaBase.cerrarNavegador();
--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -12,20 +12,22 @@ public class Hooks extends PaginaBase{
     public Hooks(){
         super(driver);
     }
-
+    // Este método se ejecuta después de cada escenario
     @After
     public void tearDown(Scenario escenario){
-        
+        // Si el escenario falló, toma una captura de pantalla
         if (escenario.isFailed()){
             escenario.log("El escenario fallo. Por favor revisar la imagen adjunta en este reporte");
             final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            // Adjuntamos la imagen al reporte del escenario
             escenario.attach(screenshot, "image/png", "Pantallazo del error");
         }
         
     }
-
+    // Este método se ejecuta después de cada paso
     @AfterStep
     public void tomarCapturaCadaPaso(Scenario escenario) {
+        // Tomamos una captura después de cada paso
         final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
             escenario.attach(screenshot, "image/png", "Pantallazo del paso");
     }
